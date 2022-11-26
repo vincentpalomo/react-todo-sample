6194cd647deb13ab62b59939d0015d3601e0f25f
vincent <vincentpalomo@yahoo.com>
2022-11-26 16:19:10 -0600

    jsx to show 2 hardcoded todos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React, { useState } from "react";
 import TodoList from "./TodoList";
 
 function App() {
+  const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
   return (
     <>
-        <TodoList />
+        <TodoList todos={todos} />
         <input type="text" />
         <button>Add Todo</button>
         <button>Clear Complete</button>
