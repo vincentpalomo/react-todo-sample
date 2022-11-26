1ef9ca7526cec44889c9bd9f6ce92983daa06af7
vincent <vincentpalomo@yahoo.com>
2022-11-26 16:43:40 -0600

    add jsx for unique id for the todo list inputs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import TodoList from "./TodoList";
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false }]
+    })
+    todoNameRef.current.value = null
+  }
+
   return (
     <>
         <TodoList todos={todos} />
-        <input type="text" />
-        <button>Add Todo</button>
+        <input ref={todoNameRef} type="text" />
+        <button onClick={handleAddTodo}>Add Todo</button>
         <button>Clear Complete</button>
         <div>0 left to do🙂</div>
     </>
