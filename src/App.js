d67529eacac0a64d393237451ab94089275a8c5e
Vincent Palomo <vincentpalomo@yahoo.com>
2022-11-27 13:33:59 -0600

    installed npm addon called styled-components and added few styling using new addon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React, { useState, useRef, useEffect } from "react";
 import TodoList from "./TodoList";
 import { v4 as uuidv4 } from 'uuid';
+import styled, { css } from "styled-components";
 
 const LOCAL_STORAGE_KEY = 'todoApp.todos'
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+
+  ${props =>
+    props.primary &&
+    css`
+      background: palevioletred;
+      color: white;
+    `};
+
+  `
 
 function App() {
   const [todos, setTodos] = useState([])
@@ -38,15 +56,19 @@ function App() {
     setTodos(newTodos)
   }
 
+  const Container = styled.div`
+  text-align: center;
+  `
+
   return (
     <>
-      <div className='todolist'>
+      <Container>
         <TodoList todos={todos} toggleTodo={toggleTodo} />
         <input ref={todoNameRef} type="text" />
-        <button onClick={handleAddTodo}>Add Todo</button>
-        <button onClick={handleClearTodos}>Clear Complete</button>
+        <Button primary onClick={handleAddTodo}>Add Todo</Button>
+        <Button onClick={handleClearTodos}>Clear Complete</Button>
         <div>{todos.filter(todo => !todo.complete).length} left to do 🙂</div>
-      </div>
+      </Container>
     </>
   )
 }
